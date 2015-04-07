8f71392a58aa539f0d3b6448226b6aae9b2a7bcf
Gary Wang <gw8471@att.com>
2015-04-07 10:08:27 -0400

    Committed by Udacity

---
+++ Final MemeMe/Meme/Meme/Meme/MyCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyCollectionViewCell.swift
+//  Meme
+//
+//  Created by Gary Wang on 4/6/15.
+//  Copyright (c) 2015 self. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+       @IBOutlet weak var imageView: UIImageView!
+}
